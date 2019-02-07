c718c2e5df6b5ba825676de604fc3395d3a6c804
Leon Hassan <leonhassan@hotmail.com>
2019-02-07 16:11:23 +0000

    Updates

---
+++ precache-manifest.35da5518952d38cfb306b87d99acf379.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "a29cc13c5515526a5193",
+    "url": "/fiori-react/static/css/main.f7083af2.chunk.css"
+  },
+  {
+    "revision": "a29cc13c5515526a5193",
+    "url": "/fiori-react/static/js/main.a29cc13c.chunk.js"
+  },
+  {
+    "revision": "d1d5125cc6660bb8e32c",
+    "url": "/fiori-react/static/css/1.fb49d0a2.chunk.css"
+  },
+  {
+    "revision": "d1d5125cc6660bb8e32c",
+    "url": "/fiori-react/static/js/1.d1d5125c.chunk.js"
+  },
+  {
+    "revision": "73367e9372318497b20d",
+    "url": "/fiori-react/static/js/runtime~main.73367e93.js"
+  },
+  {
+    "revision": "6030ef457e70912083ef3154de684dc7",
+    "url": "/fiori-react/static/media/72-Italic.6030ef45.woff2"
+  },
+  {
+    "revision": "266fd3d74665316a027af275c204537d",
+    "url": "/fiori-react/static/media/72-BoldItalic.266fd3d7.woff2"
+  },
+  {
+    "revision": "15d69c7c6c46af899033b881f0786328",
+    "url": "/fiori-react/static/media/72-Condensed.15d69c7c.woff2"
+  },
+  {
+    "revision": "0072d92bfeaf57f02de04342d06a3463",
+    "url": "/fiori-react/static/media/72-Regular.0072d92b.woff2"
+  },
+  {
+    "revision": "7f6c7cb94c0d8088b023553d46a64e07",
+    "url": "/fiori-react/static/media/72-Light.7f6c7cb9.woff2"
+  },
+  {
+    "revision": "9b4487708ef140ace9dbc396a39bbf44",
+    "url": "/fiori-react/static/media/72-Italic.9b448770.woff"
+  },
+  {
+    "revision": "85e09d643e5d4b5f98c3aaf365bd1c02",
+    "url": "/fiori-react/static/media/72-Bold.85e09d64.woff2"
+  },
+  {
+    "revision": "1f13fc6d938b5cda6bde9229f722ea8c",
+    "url": "/fiori-react/static/media/72-BoldItalic.1f13fc6d.woff"
+  },
+  {
+    "revision": "56af1d33fbd8bf1d80afd53f3f1d01ca",
+    "url": "/fiori-react/static/media/72-CondensedBold.56af1d33.woff2"
+  },
+  {
+    "revision": "c5f39800983eb3eb97ed2ba72a5be4b7",
+    "url": "/fiori-react/static/media/72-Condensed.c5f39800.woff"
+  },
+  {
+    "revision": "48cb3e07e7e509d6c81c420507d7916b",
+    "url": "/fiori-react/static/media/72-Regular.48cb3e07.woff"
+  },
+  {
+    "revision": "1e678655fe64a8a126dd51619f22edde",
+    "url": "/fiori-react/static/media/72-Bold.1e678655.woff"
+  },
+  {
+    "revision": "909707d4c2808b374157094c2950925e",
+    "url": "/fiori-react/static/media/72-CondensedBold.909707d4.woff"
+  },
+  {
+    "revision": "ab363ac31002ac1293658c71a84eef29",
+    "url": "/fiori-react/static/media/72-Light.ab363ac3.woff"
+  },
+  {
+    "revision": "c58be3ff685e0c820c8438bbf1579351",
+    "url": "/fiori-react/static/media/72-Italic.c58be3ff.ttf"
+  },
+  {
+    "revision": "3ab665b6d2fbcc13822abf07520f3972",
+    "url": "/fiori-react/static/media/72-BoldItalic.3ab665b6.ttf"
+  },
+  {
+    "revision": "c1ba0da78d7f6586cf5b00d5ef5eb806",
+    "url": "/fiori-react/static/media/SAP-icons.c1ba0da7.woff"
+  },
+  {
+    "revision": "a055c920fa3948666fc4412d72aa94f3",
+    "url": "/fiori-react/static/media/72-Regular.a055c920.ttf"
+  },
+  {
+    "revision": "317cde33cde273c643e2f4bfe788eefe",
+    "url": "/fiori-react/static/media/72-Bold.317cde33.ttf"
+  },
+  {
+    "revision": "e2f76695560366054ce11dc7efc046fd",
+    "url": "/fiori-react/static/media/72-Condensed.e2f76695.ttf"
+  },
+  {
+    "revision": "1abad19983bc369ee086769a8d503050",
+    "url": "/fiori-react/static/media/72-Light.1abad199.ttf"
+  },
+  {
+    "revision": "2bf8d215e6d4ac1b622728c4d2f06d4a",
+    "url": "/fiori-react/static/media/72-CondensedBold.2bf8d215.ttf"
+  },
+  {
+    "revision": "20f3005ee0b929d036381fccd80632aa",
+    "url": "/fiori-react/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/fiori-react/precache-manifest.35da5518952d38cfb306b87d99acf379.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/fiori-react/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
