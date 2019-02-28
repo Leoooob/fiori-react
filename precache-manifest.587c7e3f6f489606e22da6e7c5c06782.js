c75c0f6793f37c081743a875f3a0fef2054e723a
Leon Hassan <leonhassan@hotmail.com>
2019-02-28 14:28:27 +0000

    Updates

--- precache-manifest.587c7e3f6f489606e22da6e7c5c06782.js
+++ precache-manifest.587c7e3f6f489606e22da6e7c5c06782.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/fiori-react/static/media/72-Condensed.c5f39800.woff"
   },
   {
-    "revision": "b05d6247ee3332c48c75",
+    "revision": "7a8bb505f979d62e8993",
     "url": "/fiori-react/static/css/main.4a0a38d9.chunk.css"
   },
   {
@@ -40,24 +40,24 @@ self.__precacheManifest = [
     "url": "/fiori-react/static/media/72-Italic.9b448770.woff"
   },
   {
-    "revision": "7f6c7cb94c0d8088b023553d46a64e07",
-    "url": "/fiori-react/static/media/72-Light.7f6c7cb9.woff2"
-  },
-  {
     "revision": "85e09d643e5d4b5f98c3aaf365bd1c02",
     "url": "/fiori-react/static/media/72-Bold.85e09d64.woff2"
   },
   {
-    "revision": "1f13fc6d938b5cda6bde9229f722ea8c",
-    "url": "/fiori-react/static/media/72-BoldItalic.1f13fc6d.woff"
+    "revision": "7f6c7cb94c0d8088b023553d46a64e07",
+    "url": "/fiori-react/static/media/72-Light.7f6c7cb9.woff2"
   },
   {
     "revision": "56af1d33fbd8bf1d80afd53f3f1d01ca",
     "url": "/fiori-react/static/media/72-CondensedBold.56af1d33.woff2"
   },
   {
-    "revision": "b05d6247ee3332c48c75",
-    "url": "/fiori-react/static/js/main.b05d6247.chunk.js"
+    "revision": "1f13fc6d938b5cda6bde9229f722ea8c",
+    "url": "/fiori-react/static/media/72-BoldItalic.1f13fc6d.woff"
+  },
+  {
+    "revision": "7a8bb505f979d62e8993",
+    "url": "/fiori-react/static/js/main.7a8bb505.chunk.js"
   },
   {
     "revision": "48cb3e07e7e509d6c81c420507d7916b",
@@ -84,12 +84,12 @@ self.__precacheManifest = [
     "url": "/fiori-react/static/media/72-BoldItalic.3ab665b6.ttf"
   },
   {
-    "revision": "c1ba0da78d7f6586cf5b00d5ef5eb806",
-    "url": "/fiori-react/static/media/SAP-icons.c1ba0da7.woff"
-  },
-  {
     "revision": "a055c920fa3948666fc4412d72aa94f3",
     "url": "/fiori-react/static/media/72-Regular.a055c920.ttf"
+  },
+  {
+    "revision": "c1ba0da78d7f6586cf5b00d5ef5eb806",
+    "url": "/fiori-react/static/media/SAP-icons.c1ba0da7.woff"
   },
   {
     "revision": "317cde33cde273c643e2f4bfe788eefe",
@@ -108,7 +108,7 @@ self.__precacheManifest = [
     "url": "/fiori-react/static/css/1.fb49d0a2.chunk.css"
   },
   {
-    "revision": "dfce9f4b20ff524112379cb9598bfc7d",
+    "revision": "1cbd70dadd4909ada1526f4916b8fa93",
     "url": "/fiori-react/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fiori-react/precache-manifest.e416e9226f2e573c7b75526e441f95f4.js"
+  "/fiori-react/precache-manifest.587c7e3f6f489606e22da6e7c5c06782.js"
 );
 
 workbox.clientsClaim();
